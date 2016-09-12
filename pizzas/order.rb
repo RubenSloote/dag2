e719b80e2bc59615217dd29478373dfe9afdf4ff
RubenSloote <rubensloote@gmail.com>
2016-09-12 10:19:55 +0200

    first commit

---
+++ pizzas/order.rb
@@ -0,0 +1,7 @@
+def pizzas
+  {name: "BBQ", price: 6},
+  {name: "Texas", price: 7},
+  {name: "Hawai", price: 8}
+end
+
+pizzas
---
+++ pizzas/test.rb
@@ -0,0 +1,22 @@
+puts "what your name?"
+  name = gets.chomp
+    puts "> #{name}"
+
+puts "How old are you?"
+  age = gets.to_i
+    puts "> #{age}"
+
+puts "What's your gender (m/f)"
+  gender = gets.chomp
+    puts "> #{gender}"
+
+if gender == "m"
+    puts "Goodmorning Mr. #{name.capitalize}!"
+
+else
+  puts "Goordmorning Ms. #{name.capitalize}!"
+end
+
+a = 100 - age
+
+puts "You will be 100 in the year: #{a + 2016}"
